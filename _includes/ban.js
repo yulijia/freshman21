bc7e9e38837579bb9c4da5dd7c842dc259bcd0f6
yulijia <yulj2010@gmail.com>
2015-02-07 18:10:04 +0800

    add ban list

---
+++ _includes/ban.js
@@ -0,0 +1,8 @@
+<script type='text/javascript'>
+var blocklink = ['http://darodar.com','http://blackhatworth.com','http://hulfingtonpost.com','http://bestwebsitesawards.com','http://darodar.com','http://buttons-for-website.com','http://ilovevitaly.co','http://semalt.com','http://priceg.com'];
+for (var b = blocklink.length; b--;) {
+  if (document.referrer.match(blocklink[b]))
+    window.location = "http://www.google.com";
+}
+</script>
+
