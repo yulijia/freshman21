d68249b1e43595222f8aadb2a0c061bf327de885
yulijia <yulj2010@gmail.com>
2015-03-31 17:31:42 +0800

    update google search

--- _includes/google_search.js
+++
@@ -1,8 +0,0 @@
-<script language="Javascript" type="text/javascript">
-function my_search_google()
-{
-  var query = document.getElementById("my-google-search").value;
-  window.open("https://www.google.com/search?q=" + query
-      + "%20site:" + "{{site.url}}");
-}
-</script>
