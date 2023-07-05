1be9223fdc32618bdae62aff8ca1b3bcb5b77fcb
yulijia <yulj2010@gmail.com>
2023-07-05 23:12:09 +1000

    Upgrade jquery to fix 2 Dependabot alerts

--- _includes/post_pagination.js
+++ _includes/post_pagination.js
@@ -1,4 +1,4 @@
-<script src="{{ "/js/jquery-1.9.1.min.js" | prepend: site.baseurl }}"></script>
+<script src="{{ "/js/jquery-3.7.0.min.js" | prepend: site.baseurl }}"></script>
 <script type="text/javascript">
 $(function(){
   $(document).keydown(function(e) {
